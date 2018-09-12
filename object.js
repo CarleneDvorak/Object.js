5dabc29a57ca0f73310f7a65148dfdb8381fb3cc
DVORAK <cd0884213@otc.edu>
2018-09-12 10:02:00 -0500

    half way thru

---
+++ object.js
@@ -0,0 +1,32 @@
+//console.log('hello');
+
+const Aurora= {
+    name: "Aurora",
+    health: 130,
+    strength: 35,
+    xp: 15
+}
+console.log(`${Aurora.name} has ${Aurora.health} health points ${Aurora.strength} as strength and ${Aurora.xp} XP points`);
+
+const dog={
+    name: "Fang",
+    species: "boarhound dog",
+    size: 75,
+    bark: "grrr! grr!"
+}
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`);
+console.log (`Look, a cat! ${dog.name} barks: ${dog.bark}`);
+
+const account ={
+    name: "Alex",
+    balance: 0,
+     describe(){
+        return`owner: ${this.name} balance ${this.balance}`;
+    }}
+    console.log(account.describe());
+
+
+
+    //function credit (account){
+        //console.log (`owner: ${account.name} ${account.balance}`);
+    
